c548dc04e912756acd055444fb66d7cb0c4f1bd7
ThanhLongVo <vothanhlong4509@gmail.com>
2025-02-20 10:40:47 -0500

    Add files via upload

---
+++ Project/ms53/AidMan.h
@@ -0,0 +1,55 @@
+/* Citation and Sources...
+Final Project Milestone 53
+Module: AidMan
+Filename: AidMan.h
+Version 1.2
+Author	Thanh Long Vo
+Revision History
+-----------------------------------------------------------
+Date        Reason
+2023/11/15  Preliminary release
+2023/11/22  Update for milestone 51
+2023/11/24  Update for milestone 52
+2023/11/26  Update for milestone 53
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+OR
+-----------------------------------------------------------
+Write exactly which part of the code is given to you as help and
+who gave it to you, or from what source you acquired it.
+-----------------------------------------------------------*/
+
+#ifndef SDDS_AidMan_H
+#define SDDS_AidMan_H
+#include "Menu.h"
+#include "iProduct.h"
+namespace sdds
+{
+   const int sdds_max_num_items = 100;
+   class AidMan
+   {
+      char* m_fileName;
+      Menu m_mainMenu;
+      unsigned int menu() const;
+      iProduct* m_iproduct[sdds_max_num_items]{};
+      int m_numOfIproduct;
+   public:
+      AidMan();
+      AidMan(const AidMan& a) = delete;
+      AidMan& operator=(const AidMan& A) = delete;
+      virtual ~AidMan();
+      void run();
+      void save();
+      void deallocate();
+      void load();
+      void listItem();
+      void addItem();
+      void removeItem();
+      void remove(int index);
+      int list(const char* sub_desc = nullptr);
+      int search(int sku) const;
+   };
+}
+#endif
---
+++ Project/ms53/Perishable.h
@@ -0,0 +1,48 @@
+/* Citation and Sources...
+Final Project Milestone 53
+Module: Perishable
+Filename: Perishable.h
+Version 1.1
+Author	Thanh Long Vo
+Revision History
+-----------------------------------------------------------
+Date        Reason
+2023/11/18  Preliminary release
+2023/11/22  Update for milstone 51
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+OR
+-----------------------------------------------------------
+Write exactly which part of the code is given to you as help and
+who gave it to you, or from what source you acquired it.
+-----------------------------------------------------------*/
+
+#ifndef SDDS_PERISHABLE_H
+#define SDDS_PERISHABLE_H
+#include <iostream>
+#include <fstream>
+#include "Item.h"
+#include "Date.h"
+namespace sdds
+{
+   class Perishable : public Item
+   {
+      Date m_expiry;
+      char* m_instruction{};
+   public:
+      Perishable();
+      Perishable(const Perishable& P);
+      Perishable& operator=(const Perishable& P);
+      virtual ~Perishable();
+      const Date& expiry();
+      int readSku(std::istream& istr);
+      std::ofstream& save(std::ofstream& ofstr)const;
+      std::ifstream& load(std::ifstream& ifstr);
+      std::ostream& display(std::ostream& ostr)const;
+      std::istream& read(std::istream& istr);
+   };
+}
+
+#endif
