c83ef0a5572edd336c0df6cc11b6e9e04f449cd3
ThanhLongVo <vothanhlong4509@gmail.com>
2025-02-20 10:40:03 -0500

    Add files via upload

---
+++ Project/ms52/Date.h
@@ -0,0 +1,38 @@
+
+
+#ifndef SDDS_DATE_H
+#define SDDS_DATE_H
+#include <iostream>
+#include "Status.h"
+namespace sdds
+{
+   class Date
+   {
+      const int m_maxyear = 2030;
+      int m_year;
+      int m_month;
+      int m_day;
+      Status m_state;
+      bool m_format = true;
+      bool validate();
+      int uniqueDateVal();
+   public:
+      Date(const int year = 0, const int month = 0, const int date = 0);
+      Date& operator=(const Date& d);
+      bool operator==(Date& d);
+      bool operator!=(Date& d);
+      bool operator<(Date& d);
+      bool operator>(Date& d);
+      bool operator<=(Date& d);
+      bool operator>=(Date& d);
+      const Status& state()const;
+      Date& formatted(bool set);
+      operator bool()const;
+      std::ostream& write(std::ostream& ostr) const;
+      std::istream& read(std::istream& istr);
+   };
+   std::ostream& operator<<(std::ostream& ostr, const Date& d);
+   std::istream& operator>>(std::istream& istr, Date& d);
+
+}
+#endif
